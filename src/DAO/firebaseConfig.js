6c2fa739a94515ba84fb6eac593273dc54670f6c
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-10-05 20:05:56 -0300

    Conexión inicial con firebase

--- src/DAO/firebaseConfig.js
+++ src/DAO/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE,
-  authDomain: "plantilla-e9c8c.firebaseapp.com",
-  projectId: "plantilla-e9c8c",
-  storageBucket: "plantilla-e9c8c.appspot.com",
-  messagingSenderId: "628069291554",
-  appId: "1:628069291554:web:f6df8c4ef79ca454ea1c7b"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
