8e0ff6674a1afd680b3782ca7f1d90643f6c1220
Exequiel Schiavo <exelu1495@gmail.com>
2024-10-29 22:28:38 -0300

    update loginform component

--- src/app/(auth)/auth/page.jsx
+++ src/app/(auth)/auth/page.jsx
@@ -1,89 +1,28 @@
-import React from 'react';
-
-// Subcomponentes
-const BackgroundImage = () => (
-    <div
-        className="hidden bg-cover lg:block lg:w-2/3"
-        style={{ backgroundImage: "url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)" }}
-    >
-        <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
-            <div>
-                <h2 className="text-2xl font-bold text-white sm:text-3xl">TRANS AFAB</h2>
-                <p className="max-w-xl mt-3 text-gray-300">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus molestiae
-                </p>
-            </div>
-        </div>
-    </div>
-);
-
-const Logo = () => (
-    <div className="flex justify-center mx-auto">
-        <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="Logo" />
-    </div>
-);
-
-const InputField = ({ type, name, id, placeholder, label }) => (
-    <div className="mt-6">
-        <label htmlFor={id} className="block mb-2 text-sm text-gray-600 dark:text-gray-200">{label}</label>
-        <input
-            type={type}
-            name={name}
-            id={id}
-            placeholder={placeholder}
-            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-        />
-    </div>
-);
-
-const LoginButton = () => (
-    <div className="mt-6">
-        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-            Login
-        </button>
-    </div>
-);
-
-const LoginForm = () => (
-    <form>
-        <InputField type="email" name="email" id="email" placeholder="example@example.com" label="Email Address" />
-        <div className="mt-6">
-            <div className="flex justify-between mb-2">
-                <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-200">Password</label>
-                <a href="#" className="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</a>
-            </div>
-            <InputField type="password" name="password" id="password" placeholder="Your Password" label="Password" />
-        </div>
-        <LoginButton />
-    </form>
-);
-
-const SignUpPrompt = () => (
-    <p className="mt-6 text-sm text-center text-gray-400">
-        Don't have an account yet? <a href="/auth/register" className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.
-    </p>
-);
+import LoginForm from "@/app/(admin)/admin/componets/LoginForm/LoginForm"
 
 function Login() {
     return (
         <div className="bg-white dark:bg-gray-900">
             <div className="flex justify-center h-screen">
-                <BackgroundImage />
-                <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
-                    <div className="flex-1">
-                        <div className="text-center">
-                            <Logo />
-                            <p className="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
-                        </div>
-                        <div className="mt-8">
-                            <LoginForm />
-                            <SignUpPrompt />
+                <div className="hidden bg-cover lg:block lg:w-2/3" style={{ backgroundImage: "url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)" }}>
+                    <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+                        <div>
+                            <h2 className="text-2xl font-bold text-white sm:text-3xl">Inkua</h2>
+
+                            <p className="max-w-xl mt-3 text-gray-300">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
+                                autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus
+                                molestiae
+                            </p>
                         </div>
                     </div>
+
                 </div>
+                    <LoginForm/>
+
             </div>
         </div>
-    );
+    )
 }
 
-export default Login;
+export default Login
