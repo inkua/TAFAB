879cc49dd1d9ca347c277836a1621c71d9f0b1a3
joelwaiman <waimanjoel@gmail.com>
2024-11-28 12:25:23 -0300

    add: drag and drop

--- src/app/(admin)/admin/products/components/FormProduct/FormProduct.jsx
+++ src/app/(admin)/admin/products/components/FormProduct/FormProduct.jsx
@@ -10,18 +10,22 @@ function FormProduct({ isOpen, setIsOpen, saveData, data, add = true }) {
     const [description, setDescription] = useState("");
     const [id, setId] = useState("");
     const [image, setImage] = useState(null);
+    const [imagePreview, setImagePreview] = useState(null)
 
     const handleChangeImage = (e) => {
-        if(e.target.files[0]){
-            setImage(e.target.files[0])
+        if (e.target.files[0]) {
+            const file = e.target.files[0];
+            setImage(file);
+            setImagePreview(URL.createObjectURL(file));
         }
-    }
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         let imageUrl = ""
-        
-        if (image){
+
+        if (image) {
             imageUrl = await uploadImage(image, 'products')
         }
 
@@ -47,6 +51,51 @@ function FormProduct({ isOpen, setIsOpen, saveData, data, add = true }) {
 
         setIsOpen(false)
     }
+
+    //DRAG AND DROP
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
+    const handleDragEnter = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.currentTarget.classList.add("bg-gray-200");
+    };
+
+    const handleDragLeave = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.currentTarget.classList.remove("bg-gray-200");
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.currentTarget.classList.remove("bg-gray-200");
+
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            const file = e.dataTransfer.files[0];
+            setImage(file);
+            setImagePreview(URL.createObjectURL(file));
+        }
+    };
+
+    //CLEAR FORM
+
+    const handleClear = () => {
+        setName('');
+        setPrice('');
+        setCategory('');
+        setDescription('');
+        setImage(null);
+        setImagePreview(null);
+        setId('');
+        setIsOpen(false); // También cierra el formulario
+    };
+
 
     useEffect(() => {
         if (data) {
@@ -140,37 +189,73 @@ function FormProduct({ isOpen, setIsOpen, saveData, data, add = true }) {
                                 </div>
 
 
-                                <div className="flex items-center justify-center w-full">
-                                    <label for="dropzone-file"
-                                        class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-200 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500">
-                                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 20 16">
-                                                <path stroke="currentColor"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
-                                                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                            </svg>
-                                            <p className="mb-2 text-sm text-gray-800 dark:text-gray-400">
-                                                <span className="font-semibold">Click to upload</span>
-                                                or drag and drop</p>
-                                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                                        </div>
-                                        <input id="dropzone-file"
+                                <div
+                                    className="flex items-center justify-center w-full"
+                                    onDragOver={handleDragOver}
+                                    onDragEnter={handleDragEnter}
+                                    onDragLeave={handleDragLeave}
+                                    onDrop={handleDrop}
+                                >
+                                    <label
+                                        htmlFor="dropzone-file"
+                                        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                                    >
+                                        {imagePreview ? (
+                                            <div className='relative z-10'>
+                                                <img
+                                                    src={imagePreview}
+                                                    alt="Preview"
+                                                    className="object-contain w-full h-full rounded-lg"
+                                                />
+                                                <button
+                                                    className="absolute flex items-center justify-center top-3 right-3 bg-gray-400/50 hover:bg-slate-300/75 transition-colors duration-200 h-10 w-10 rounded-full text-center"
+                                                    onClick={(e) => {
+                                                        setImagePreview(null);
+                                                    }}
+                                                >
+                                                    <span className="text-black font-bold">X</span>
+                                                </button>
+                                            </div>
+
+                                        ) : (
+                                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                                <svg
+                                                    className="w-8 h-8 mb-4 text-gray-500"
+                                                    aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 20 16"
+                                                >
+                                                    <path
+                                                        stroke="currentColor"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                                                    />
+                                                </svg>
+                                                <p className="mb-2 text-sm text-gray-800">
+                                                    <span className="font-semibold">Click to upload</span> or drag
+                                                    and drop
+                                                </p>
+                                                <p className="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                            </div>
+                                        )}
+                                        <input
+                                            id="dropzone-file"
                                             type="file"
-                                            accept='image/*'
+                                            accept="image/*"
                                             onChange={handleChangeImage}
-                                            className="hidden" />
+                                            className="hidden"
+                                        />
                                     </label>
                                 </div>
+
                             </div>
 
                             <div className="flex justify-end space-x-4 p-6 border-t">
                                 <button
-                                    onClick={() => setIsOpen(false)}
+                                    onClick={handleClear}
                                     type='button'
                                     className="px-4 py-2 border border-gray-300 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                                 >
