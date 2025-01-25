4a93360ae11303614a2cc51e1a00ac05c625965b
mauroradino <mauroradino2003@gmail.com>
2025-01-25 00:28:08 -0300

    informate page

--- src/app/(main)/informate/page.jsx
+++ src/app/(main)/informate/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import News_card from './components/news_card'
 
 function Informate() {
     return (
-        <main>
-            <h1>Infórmate</h1>
+        <main className='flex flex-col px-[24px] pt-[64px] pb-[70px] gap-[30px] lg:px-[150px]'>
+            <h1 className='font-[900] text-[25px]'>INFORMACIÓN DEL MOVIMIENTO TRANS</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut numquam excepturi dolorum sequi placeat. Sequi minima officia nam? Voluptas optio consequuntur quasi et, est aut beatae adipisci temporibus eligendi velit.</p>
+            <News_card img="/second_news.svg" date="Julio 11, 2025"/>
+            <News_card img="/first_news.svg" date="Julio 11, 2025"/>
+            <News_card img="/third_news.svg" date="Julio 11, 2025"/>
+            <News_card img="/fourth_news.svg" date="Julio 11, 2025"/>
         </main>
     )
 }
