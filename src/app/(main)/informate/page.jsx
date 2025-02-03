d84334551fe73daab539914193a5b96aeba7fa2e
mauroradino <mauroradino2003@gmail.com>
2025-02-03 11:20:24 -0300

    cambios en pagina noticias individuales y enlaces de grupales

--- src/app/(main)/informate/page.jsx
+++ src/app/(main)/informate/page.jsx
@@ -6,10 +6,10 @@ function Informate() {
         <main className='flex flex-col px-[24px] pt-[64px] pb-[70px] gap-[30px] lg:px-[150px]'>
             <h1 className='font-[900] text-[25px]'>INFORMACIÓN DEL MOVIMIENTO TRANS</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut numquam excepturi dolorum sequi placeat. Sequi minima officia nam? Voluptas optio consequuntur quasi et, est aut beatae adipisci temporibus eligendi velit.</p>
-            <News_card img="/second_news.svg" date="Julio 11, 2025"/>
-            <News_card img="/first_news.svg" date="Julio 11, 2025"/>
-            <News_card img="/third_news.svg" date="Julio 11, 2025"/>
-            <News_card img="/fourth_news.svg" date="Julio 11, 2025"/>
+            <News_card href="/infoNoticia" img="/second_news.svg" date="Julio 11, 2025"/>
+            <News_card href="/infoNoticia" img="/first_news.svg" date="Julio 11, 2025"/>
+            <News_card href="/infoNoticia" img="/third_news.svg" date="Julio 11, 2025"/>
+            <News_card href="/infoNoticia" img="/fourth_news.svg" date="Julio 11, 2025"/>
         </main>
     )
 }
